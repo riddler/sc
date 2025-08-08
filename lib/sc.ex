defaf05b818e358d3f2ecaff6d5ca4519d347164
JohnnyT <ubergeek3141@gmail.com>
2025-08-08 17:56:59 -0600

    Adds files from generating new mix application

---
+++ lib/sc.ex
@@ -0,0 +1,18 @@
+defmodule SC do
+  @moduledoc """
+  Documentation for `SC`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SC.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
