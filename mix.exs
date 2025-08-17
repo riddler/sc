531476cc11ab09d90a7ee3f3ccbc6f17a30d3de2
JohnnyT <ubergeek3141@gmail.com>
2025-08-17 07:38:03 -0600

    Adds additional CI fixes

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule SC.MixProject do
     [
       app: @app,
       version: @version,
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: @deps,
